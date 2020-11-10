b5d65564ef4bd6bd9cd222182c6960b47ebe5c44
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2020-11-10 05:18:32 +0000

    [Start]bookers4がエラーで解決できない為

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books
+  attachment :profile_image, destroy: false
+
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+  has_many :user_rooms
+  has_many :chats
+  has_many :rooms, through: :user_rooms
+end
